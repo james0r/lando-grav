292c8647d79491f75b083a315083077ada4c72d7
James <james.auble@gmail.com>
2024-03-26 22:08:28 +0900

    added installer script

--- installer.sh
+++ installer.sh
@@ -3,8 +3,15 @@
 # Start Lando
 lando start
 
+sleep 5
+
 # Run Lando commands
 lando install-grav
 lando rebuild -y
 lando install-gravified
 lando install-grav-theme-vite
+lando install-grav-theme-vite-npm-deps
+lando build
+lando start
+
+# theme activation
